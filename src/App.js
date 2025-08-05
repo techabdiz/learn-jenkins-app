5edc71cf956f1a78b2ecca18e51263511ff219e6
Aziz <tech.abdiz@gmail.com>
2025-08-06 00:23:18 +0530

    testing git poller by adding changes

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Some changes to this application
         </a>
       </header>
       <p>
-          Application version: 1
+          Application version: 2
       </p>
     </div>
   );
