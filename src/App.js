d89258330dd7caeaf576dc8eaa0b0fc51471865a
Aziz <tech.abdiz@gmail.com>
2025-08-06 00:26:46 +0530

    reverted for test fix

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Some changes to this application
+          Learn Jenkins on Udemy
         </a>
       </header>
       <p>
-          Application version: 2
+          Application version: 1
       </p>
     </div>
   );
