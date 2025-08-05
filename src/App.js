82abcc9fc752108570ac43f999fcee62e7ef779a
Aziz <tech.abdiz@gmail.com>
2025-08-06 03:34:16 +0530

    small change in app.ks

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Dont Learn Jenkins on Udemy
         </a>
       </header>
       <p>
