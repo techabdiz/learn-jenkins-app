ffb6e9bd70a936af9c6b10f9145bff208e14b780
Aziz <tech.abdiz@gmail.com>
2025-08-06 00:31:16 +0530

    fixed test cases

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          This is my website now
         </a>
       </header>
       <p>
